589d8fe88c2bfee0be031389a7848f62a01b2183
Zaid Tariq Shah <zaidtariqshah@gmail.com>
2026-05-12 22:23:43 +0530

    secondValidationCheck(minor)

---
+++ Vcheck.js
@@ -0,0 +1,33 @@
+import http from "http";
+
+const Validation = (data, res) => {
+  if (!data.email || !data.password) {
+    res.writeHead(400, "Bad Boy", { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "Email and Password are required" }));
+    return false;
+  }
+  return true;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.method === "POST" && req.url.startsWith("/checking")) {
+    let body = "";
+    req.on("data", (dataChunks) => {
+      body += dataChunks;
+    });
+    req.on("end", () => {
+      const parsedData = JSON.parse(body);
+      console.log(parsedData);
+      const isValid = Validation(parsedData, res);
+      if (!isValid) res.end("Checking !Sign");
+      res.writeHead(200, "Success", { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Message Received Successfully",
+          data: parsedData,
+        }),
+      );
+    });
+  }
+});
+server.listen(9009, () => console.log("Running PORT 9009"));
